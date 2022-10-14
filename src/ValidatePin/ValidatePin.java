f9c492a2a034948ac84f1162b7cc12369a1455bb
owenprotz <113632837+owenprotz@users.noreply.github.com>
2022-10-14 14:38:13 -0400

    loop

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int pin = 4200;
+        System.out.println("Enter a PIN");
+        int userPin=in.nextInt();
+        int userGuesses=4;
+        while (userPin!=pin) {
+        	System.out.println("invalid PIN. Try again.");
+        	if (userGuesses>3)  {
+        		break;
+        	}
+        	userGuesses++;
+        	userPin=in.nextInt();
+        }
+        if (userPin!=pin) {
+        	System.out.println("You ran out of guesses. Police have been notified");
+        }else {
+        	System.out.println("PIN correct. Access granted");
+        }
         
+        		
     }
 }
