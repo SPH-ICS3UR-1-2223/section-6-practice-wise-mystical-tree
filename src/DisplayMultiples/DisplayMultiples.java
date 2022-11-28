1c48688127a329374e3ac93591b91c8841d28ae0
owenprotz <owen.protz@stu.ocsb.ca>
2022-11-28 14:26:32 -0500

    multiples done

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class DisplayMultiples {
 
-    public static void main(String args[]) {
+    private static int counter;
+
+	public static void main(String args[]) {
+       Scanner in = new Scanner(System.in);
+       //
+       System.out.println("pick a number");
+       int user = in.nextInt();
        
+       for (int counter = 1; counter <= 12; counter++);
+       System.out.println(counter * user);
     }
 
 }
